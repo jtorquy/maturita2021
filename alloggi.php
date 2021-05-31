00d6c899042757f18b48cb1115520294090740f1
torqua <51115841+jtorquy@users.noreply.github.com>
2021-05-31 20:09:28 +0200

    Update and rename alloggi.html to alloggi.php

---
+++ alloggi.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="stile.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Roboto:wght@300;900&display=swap" rel="stylesheet">
+    <title> Alloggi – Vastour</title>
+</head>
+
+<body>
+    <header>
+        <nav class="nav">
+            <div class="logo">
+                <h4><a href="index.php" class="vastour">vastour</a></h4>
+            </div>
+            <ul class="nav_links">
+                <li>
+                    <a href="scopri.php" class="nav_item ">Scopri</a>
+                </li>
+                <li>
+                    <a href="eventi.php " class="nav_item ">Eventi</a>
+                </li>
+                <li>
+                    <a href="alloggi.php " class="nav_item current">Alloggi</a>
+                </li>
+                <li>
+                    <a href="raggiungici.php " class="nav_item ">Raggiungici</a>
+                </li>
+                <li>
+                    <a href="info.php " class="nav_item ">Info</a>
+                </li>
+            </ul>
+            <div class="burger">
+                <div class="line1 "></div>
+                <div class="line2 "></div>
+                <div class="line3 "></div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="content alloggi">
+            <div class="content_text two">
+                <h4>Alloggi</h4>
+            </div>
+        </div>
+
+        <div class="wrapper">
+        <div class="box img"><img src="./images/caravel.jpg" /></div>
+            <div class="box span-2">
+                <h5>Hotel Caravel</h5>
+                <p>Ubicato a 2 minuti a piedi dalla spiaggia, questo hotel confortevole e aperto solo durante il periodo estivo si trova a 2,5 km dalla stazione ferroviaria di Vasto San Salvo e a 2,6 km dalla Cattedrale di Vasto.<br/><br/> Le camere, sobrie
+                    e arredate in modo semplice, sono dotate di WiFi gratuito e TV. È disponibile il servizio in camera. Colazione a buffet, sedie a sdraio nella spiaggia privata, attività per bambini e parcheggio sono inclusi nel prezzo. <br/><br/>La
+                    struttura comprende ristorante informale, bar, area lounge nella hall con TV e reception aperta 24 ore al giorno. La disponibilità è soggetta a regole di soggiorno minimo.</p>
+                    <a class="prenota" href="#form">Prenota ora</a>
+                </div>
+            
+            <div class="box span-2">
+                <h5>Hotel dei 7</h5>
+                <p>Questo hotel sobrio, ospitato in un edificio d'epoca tipico della riviera, dista 3 km dal mare, 5,5 km dall'Aqualand di Vasto e 11 km dalla Riserva Naturale di Punta Aderci.
+                <br/><br/>
+Le camere e gli appartamenti, in stile semplice e con pavimento in parquet, sono dotati di WiFi gratuito, TV satellitare, balcone e, in alcuni casi, godono di vista sul mare. Gli appartamenti comprendono letto aggiuntivo e angolo cottura con frigorifero. È disponibile il servizio in camera.
+<br/><br/>
+La colazione, inclusa, viene servita in una sala da pranzo a tinte vivaci. La struttura comprende reception aperta 24 ore al giorno, bar e palestra. I servizi a pagamento includono parcheggio e servizio navetta per la stazione ferroviaria. Si accettano animali domestici.</p>
+<a class="prenota" href="#form">Prenota ora</a>
+            </div>
+            <div class="box img"><img src="./images/hoteldeisette.jpg" /></div>
+            <div class="box img"><img src="./images/adriatico.jpg" /></div>
+            <div class="box span-2">
+                <h5>Hotel Adriatico</h5>
+                <p>Situato in una strada residenziale, questo hotel confortevole dista 5 minuti a piedi dalla spiaggia, 4 km da Vasto e 8 km dal parco Aqualand del Vasto.
+                <br/><br/>
+Le camere funzionali e insonorizzate sono dotate di WiFi gratuito, TV a schermo piatto, frigobar e balcone. Le sistemazioni di categoria superiore offrono in più divano letto e vassoio di cortesia con tè e caffè.
+<br/><br/>
+La colazione a buffet è inclusa nel prezzo e viene servita in una sala da pranzo luminosa. La struttura comprende inoltre ristorante di cucina abruzzese, bar, piscina scoperta con solarium, programma di animazione e lido privato in spiaggia dotato di sedie e lettini prendisole. Si accettano animali domestici.</p>
+<a class="prenota" href="#form">Prenota ora</a>
+            </div>
+            <div class="box span-2">
+                <h5>Hotel San Giorgio</h5>
+                <p>Situato a 2 minuti a piedi dalla spiaggia, questo hotel sobrio dista 2,3 km dalla stazione ferroviaria di Vasto San Salvo e 3 km dal Castello Caldoresco.
+                <br/><br/>
+Le camere informali sono dotate di WiFi gratuito, TV satellitare, frigobar e balcone. È disponibile il servizio in camera.
+<br/><br/>
+La colazione e il parcheggio sono inclusi nel prezzo. La struttura comprende inoltre ristorante essenziale, bar informale e spiaggia privata.</p>
+<a class="prenota" href="#form">Prenota ora</a>
+</div>
+            <div class="box img"><img src="./images/hotel-san-giorgio.jpg" /></div>
+            <div class="box img"><img src="./images/hotel-bristol.jpg" /></div>
+            <div class="box span-2">
+                <h5>Hotel Bristol</h5>
+                <p>Questo hotel confortevole dista 1 minuto a piedi dalla spiaggia più vicina sulla costa adriatica, 3 km sia dal rinascimentale Palazzo d'Avalos, sede di musei e giardini, sia dalla stazione ferroviaria di Vasto-San Salvo e 11 km dalla Riserva Naturale Punta Aderci.
+                <br/><br/>
+Le camere semplici sono dotate di vassoio di cortesia con tè e caffè, WiFi gratuito, TV satellitare, acqua minerale, frigobar e balcone. L'aria condizionata è disponibile a orari stabiliti. Alcune sistemazioni godono di vista sul mare e possono ospitare fino a tre persone.
+<br/><br/>
+La colazione e il parcheggio sono inclusi nel prezzo. La struttura comprende ristorante formale, area lounge e spiaggia privata con lettini prendisole e sedie a sdraio.</p>
+<a class="prenota" href="#form">Prenota ora</a>
+            </div>
+            <div class="box all">
+                <a name="form"> </a>
+                <form name="formAlloggi" method="post" action="prenotaalloggi.php">
+                    <div class="form elem-group inlined">
+                        <label for="nome">Nome: </label>
+                        <input type="text" name="nome" id="nome" placeholder="Miriana" required>
+                    </div>
+                    <div class="elem-group inlined">
+                        <label for="cognome">Cognome: </label>
+                        <input type="text" name="cognome" id="cognome" placeholder="Di Sabato" required>
+                    </div>
+                    <div class="elem-group">
+                        <label for="email">Email: </label>
+                        <input type="text" name="email" id="email" placeholder="mirianadisabato@email.com" required>
+                    </div>
+                    <div class="elem-group">
+                        <label for="numTel">Numero: </label>
+                        <input type="text" name="numTel" id="numTel" placeholder="345 9561305" required>
+                    </div>
+                    <hr>
+                    <div class="elem-group">
+                        <label for="alloggio">Alloggio: </label>
+                        <select name="alloggio">
+                            <option value="1">Hotel Caravel</option>
+                            <option value="2">Hotel dei 7</option>
+                            <option value="3">Hotel Adriatico</option>
+                            <option value="4">Hotel San Giorgio</option>
+                            <option value="5">Hotel Bristol</option>
+                        </select>
+                    </div>
+                    <div class=" elem-group inlined">
+    <label for="checkin">Check-in</label>
+    <input type="date" id="checkin" name="checkin" required>
+  </div>
+  <div class="elem-group inlined">
+    <label for="checkout">Check-out</label>
+    <input type="date" id="checkout" name="checkout" required>
+  </div>
+  <div>
+    <label for="stanza">Seleziona una stanza:</label>
+    <select id="stanza" name="stanza" required>
+        <option value="">Scegli una stanza dalla lista</option>
+        <option value="singola">singola</option>
+        <option value="doppia">doppia</option>
+    </select>
+  </div>
+  <button class="prenota2"type="submit">Prenota</button>
+                </form>
+
+            </div>
+        </div>
+
+    </main>
+
+    <footer class="footer-main">
+        <div class="footer-right">
+            <a href="https://www.facebook.com/jtorquy"><i class="fa fa-facebook"></i></a>
+            <a href="https://github.com/jtorquy"><i class="fa fa-github"></i></a>
+            <a href="https://it.linkedin.com/in/lorenzo-jetrin-torquato-5a957617a"><i class="fa fa-linkedin"></i></a>
+            <a href="https://www.paypal.com/paypalme/torquaa"><i class="fa fa-paypal"></i></a>
+        </div>
+        <div class="footer-left">
+
+            <p class="footer-links">
+                <a class="link-1" href="scopri.php">Scopri</a>
+                <a href="eventi.php">Eventi</a>
+                <a href="alloggi.php">Alloggi</a>
+                <a href="raggiungici.php">Raggiungici</a>
+                <a href="info.php">Info</a>
+            </p>
+            <p>VASTOUR &copy; 2021 <br/>> made by Lorenzo Jetrin Torquato</p>
+            <p></p>
+        </div>
+    </footer>
+
+    <script src="script.js "></script>
+</body>
+
+</html>
