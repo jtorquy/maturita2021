2e6562e49a0be657b2a42fd3c8d79ebf24493603
torqua <51115841+jtorquy@users.noreply.github.com>
2021-05-31 20:09:47 +0200

    Update and rename eventi.html to eventi.php

---
+++ eventi.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="stile.css">
+    <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Roboto:wght@300;900&display=swap" rel="stylesheet">
+    <title> Eventi – Vastour</title>
+</head>
+
+<body>
+    <header>
+        <nav class="nav">
+            <div class="logo">
+                <h4><a href="index.php" class="vastour">vastour</a></h4>
+            </div>
+            <ul class="nav_links">
+                <li>
+                    <a href="scopri.php" class="nav_item ">Scopri</a>
+                </li>
+                <li>
+                    <a href="eventi.php " class="nav_item current">Eventi</a>
+                </li>
+                <li>
+                    <a href="alloggi.php " class="nav_item ">Alloggi</a>
+                </li>
+                <li>
+                    <a href="raggiungici.php " class="nav_item ">Raggiungici</a>
+                </li>
+                <li>
+                    <a href="info.php " class="nav_item ">Info</a>
+                </li>
+            </ul>
+            <div class="burger">
+                <div class="line1 "></div>
+                <div class="line2 "></div>
+                <div class="line3 "></div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="content eventi">
+            <div class="content_text two">
+                <h4>Eventi</h4>
+            </div>
+        </div>
+
+        <div class="wrapper">
+        <div class="box img"><img src="./images/vasto-cs.jpg" /></div>
+            <div class="box span-2">
+                <h5>L'altra Vasto – Passeggiata nel centro storico<br/><em>16-19 giugno</em></h5>
+                <p>La camminata si svolgerà tra le vie del centro storico fra le bellezze artistiche meno conosciute della città. Per l'occasione, sarà possibile ammirare luoghi che normalmente non sono aperti al pubblico. Si visiterà la chiesa di Santa Filomena, il giardino del Palazzo Genova Rulli di Via Anelli, la chiesa di San Teodoro (ex patrono di Vasto), la chiesa della Santissima Trinità e la Torre Diomede Del Moro.<br/><br/> La camminata è giunta alla seconda edizione e, visto il grande riscontro di pubblico della passata stagione e visti i tanti messaggi di stima e di incoraggiamento che stiamo ricevendo in questi giorni, si è deciso di riproporre a beneficio dei vastesi e dei turisti questo evento, con la speranza che possa diventare, per i prossimi anni, un appuntamento fisso delle estati vastesi.<br/><br/>La partecipazione all'evento è gratuita. Data la disponibilità limitata dei posti, è richiesta la prenotazione obbligatoria. Inoltre, si avverte che alcuni edifici storici presentano lievi difficottà di accesso e barriere architettoniche. Si consiglia di indossare calzature comode</p>
+                <a class="prenota" href="#form">Prenota ora</a>
+            </div>
+            <div class="box span-2">
+                <h5>WineVasto – Degustazione vini<br/><em>7 luglio</em></h5>
+                <p>La serata, organizzata dal Fantini Store di Stefania Sabatini e con la collaborazione di Gianluca Pagano, sarà l’opportunità per soddisfare la curiosità di quanti vogliano conoscere più da vicino la realtà vinicola locale, dialogando con i suoi stessi attori.
+<br/><br/>
+Iasci e Marchesani, Fontefico, San Michele, Iasci e la Farnese by Fantini saranno presenti con i loro stand dove sarà possibile degustare i vini autoctoni, accompagnati da prodotti tipici locali, preparati dai ristoranti presenti nella zona che ospiterà l’evento.
+<br/><br/>
+Non mancherà l’intrattenimento musicale ad allietare il corso della manifestazione.
+<br/><br/>
+L’intento non solo è quello promuovere le risorse enogatronomiche locali, ma di trasmettere, attraverso un’occasione di convivialità, i valori e la cultura del territorio inerenti a uno delle sue più grandi realtà rappresentative, il vino.</p>
+<a class="prenota" href="#form">Prenota ora</a>
+</div>
+            <div class="box img"><img src="./images/winevasto.jpg" /></div>
+            <div class="box img"><img src="./images/ventricina.jpg" /></div>
+            <div class="box span-2">
+                <h5>Festival della ventricina<br/><em>12-14 luglio</em></h5>
+                <p>Il Festival si articolerà in una parte espositiva e in una ristorativa, che si svolgeranno a partire dalle 18.30 in piazza Pudente e in piazza del Popolo, le due eleganti aree attorno a Palazzo D’Avalos, che accoglierà a sua volta nei suoi giardini e nel suo chiostro incontri ed eventi collaterali. <br/><br/>Nei colorati stand sarà possibile degustare la ventricina in proposte semplici e dirette, ideali magari per un aperitivo. Oltre ai produttori locali, saranno presenti anche altri espositori con il meglio della produzione enogastronomica di qualità del territorio.<br/><br/> Nell’area ristorazione, invece, sarà possibile scoprire la versatilità della ventricina in cucina, con intriganti e sorprendenti menu a tema ideati per l’occasione: dall’antipasto ai primi ai secondi, sarà l’occasione per assistere alla sublimazione del salume tradizionale vastese, grazie alla creatività di produttori e chef. Completano l’offerta un ricco cartellone di spettacoli, show cooking, laboratori del gusto, visite guidate, convegni, mostre e altro.</p>
+                <a class="prenota" href="#form">Prenota ora</a>
+            </div>
+            <div class="box span-2">
+                <h5>Siren Festival<br/><em>26-29 luglio</em></h5>
+                <p>Dopo la pausa del 2020 e l'impossibilità di realizzare l'edizione del 2019, 'Road to Siren' è un evento voluto fortemente che, in questa strana estate, vuole rappresentare simbolicamente la ripartenza, passo dopo passo per riscoprire la sinergia tra il Siren Festival e Vasto (fra le 50 più belle spiagge d'Europa).
+                <br/><br/>
+La Sirena torna a cantare in quella magnifica cornice che è il comune abruzzese: uno scenario perfetto, come molti negli anni passati hanno descritto, in cui non solo vivere le emozioni di un concerto ma soprattutto quella di un'esperienza diversa, unica. Un festival che non vuole essere solo festival ma un momento da attraversare, se possibile, con tutti i sensi.</p>
+<a class="prenota" href="#form">Prenota ora</a></div>
+            <div class="box img"><img src="./images/sirenfestival.jpg" /></div>
+
+            <div class="box all">
+                <a name="form"> </a>
+                <form name="formAlloggi" method="post" action="prenotaeventi.php">
+                    <div class="form elem-group inlined">
+                        <label for="nome">Nome: </label>
+                        <input type="text" name="nome" id="nome" placeholder="Miriana" required>
+                    </div>
+                    <div class="elem-group inlined">
+                        <label for="cognome">Cognome: </label>
+                        <input type="text" name="cognome" id="cognome" placeholder="Di Sabato" required>
+                    </div>
+                    <div class="elem-group">
+                        <label for="email">Email: </label>
+                        <input type="text" name="email" id="email" placeholder="mirianadisabato@email.com" required>
+                    </div>
+                    <div class="elem-group">
+                        <label for="numTel">Numero: </label>
+                        <input type="text" name="numTel" id="numTel" placeholder="345 9561305" required>
+                    </div>
+                    <hr>
+                    <div class="elem-group">
+                        <label for="evento">Evento: </label>
+                        <select name="evento">
+                            <option value="1">L'altra Vasto – Passeggiata nel centro storico 	</option>
+                            <option value="2">WineVasto – Degustazione vini</option>
+                            <option value="3">Festival della ventricina</option>
+                            <option value="4"> 	Siren Festival</option>
+                        </select>
+                    </div>
+                    
+  <button class="prenota2"type="submit">Prenota</button>
+                </form>
+
+            </div>
+        </div>
+    </main>
+
+    <footer class="footer-main">
+        <div class="footer-right">
+            <a href="https://www.facebook.com/jtorquy"><i class="fa fa-facebook"></i></a>
+            <a href="https://github.com/jtorquy"><i class="fa fa-github"></i></a>
+            <a href="https://it.linkedin.com/in/lorenzo-jetrin-torquato-5a957617a"><i class="fa fa-linkedin"></i></a>
+            <a href="https://www.paypal.com/paypalme/torquaa"><i class="fa fa-paypal"></i></a>
+        </div>
+        <div class="footer-left">
+
+        <p class="footer-links">
+                <a class="link-1" href="scopri.php">Scopri</a>
+                <a href="eventi.php">Eventi</a>
+                <a href="alloggi.php">Alloggi</a>
+                <a href="raggiungici.php">Raggiungici</a>
+                <a href="info.php">Info</a>
+            </p>
+            <p>VASTOUR &copy; 2021 <br/>> made by Lorenzo Jetrin Torquato</p>
+            <p></p>
+        </div>
+    </footer>
+
+    <script src="script.js "></script>
+</body>
+
+</html>
