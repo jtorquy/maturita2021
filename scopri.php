71ebf220df9ba3d4c5668bca2cba7dbcd2cd8287
torqua <51115841+jtorquy@users.noreply.github.com>
2021-05-31 20:10:32 +0200

    Update and rename scopri.html to scopri.php

--- scopri.php
+++ scopri.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="stile.css">
-    <link rel="stylesheet" href="/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Roboto:wght@300;900&display=swap" rel="stylesheet">
@@ -17,23 +17,23 @@
     <header>
         <nav class="nav">
             <div class="logo">
-                <h4><a href="index.html" class="vastour">vastour</a></h4>
+                <h4><a href="index.php" class="vastour">vastour</a></h4>
             </div>
             <ul class="nav_links">
                 <li>
-                    <a href="scopri.html" class="nav_item current">Scopri</a>
+                    <a href="scopri.php" class="nav_item current">Scopri</a>
                 </li>
                 <li>
-                    <a href="eventi.html " class="nav_item ">Eventi</a>
+                    <a href="eventi.php " class="nav_item ">Eventi</a>
                 </li>
                 <li>
-                    <a href="alloggi.html " class="nav_item ">Alloggi</a>
+                    <a href="alloggi.php " class="nav_item ">Alloggi</a>
                 </li>
                 <li>
-                    <a href="raggiungici.html " class="nav_item ">Raggiungici</a>
+                    <a href="raggiungici.php " class="nav_item ">Raggiungici</a>
                 </li>
                 <li>
-                    <a href="info.html " class="nav_item ">Info</a>
+                    <a href="info.php " class="nav_item ">Info</a>
                 </li>
             </ul>
             <div class="burger">
@@ -52,7 +52,7 @@
         </div>
 
         <div class="wrapper">
-            <div class="box img"><img src="/images/vasto2.jpg" /></div>
+            <div class="box img"><img src="./images/vasto2.jpg" /></div>
             <div class="box span-2">
                 <h5>Centro storico</h5>
                 <p>Il centro storico di Vasto costituisce tutt'ora uno dei quartieri maggiori e più frequentati della città.<br/><br/> Oltre alle numerose chiese presenti, come la Cattedrale di San Giuseppe (XII sec.) e la Chiesa di Santa Magia Maggiore,
@@ -67,8 +67,8 @@
                     di Cesare Michelangelo d’Avalos, venne nominato comandante delle truppe di casa d’Avalos, e fu il proprietario di numerose abitazioni poste lungo il percorso che poi prenderà il suo nome.<br/><br/>Sia dal Belvedere che dalla Loggia
                     è possibile godersi uno spettacolare panorama sul Golfo di Vasto: si riesce a vedere l'intero quartiere di Vasto Marina e alcuni comuni limitrofi, come San Salvo o Termoli.<br/><br/><em>A destra, il Belvedere.</em></p>
             </div>
-            <div class="box img"><img src="/images/0002.jpg" /></div>
-            <div class="box img"><img src="/images/beach-vasto-marina.jpg" /></div>
+            <div class="box img"><img src="./images/0002.jpg" /></div>
+            <div class="box img"><img src="./images/beach-vasto-marina.jpg" /></div>
             <div class="box span-2">
                 <h5>Vasto Marina</h5>
                 <p>Vasto Marina è uno dei quartieri più turistici della città.<br/><br/>Frequentata soprattutto durante il periodo estivo, offre una lunga passeggiata folta di negozi, ristoranti, alberghi e bar, fulcro della vita notturna della città.<br/><br/>Parallelamente
@@ -81,7 +81,7 @@
                     avviene tramite delle assi di legno che portano ad una grande spiaggia sabbiosa, affiancata da un mare in cui si poggiano diversi trabocchi.<br/><br/>Zona portuale, il faro di Punta Penna con i suoi 70m di altezza è il secondo più
                     alto in Italia, dopo la Lanterna di Genova.<br/><br/><em>A destra, il faro di Punta Penna e la chiesa di Santa Maria di Pennaluce.</em></p>
             </div>
-            <div class="box img"><img src="/images/shutterstock_453734107.jpg" /></div>
+            <div class="box img"><img src="./images/shutterstock_453734107.jpg" /></div>
         </div>
     </main>
 
@@ -94,12 +94,12 @@
         </div>
         <div class="footer-left">
 
-            <p class="footer-links">
-                <a class="link-1" href="scopri.html">Scopri</a>
-                <a href="eventi.html">Eventi</a>
-                <a href="alloggi.html">Alloggi</a>
-                <a href="raggiungici.html">Raggiungici</a>
-                <a href="info.html">Info</a>
+        <p class="footer-links">
+                <a class="link-1" href="scopri.php">Scopri</a>
+                <a href="eventi.php">Eventi</a>
+                <a href="alloggi.php">Alloggi</a>
+                <a href="raggiungici.php">Raggiungici</a>
+                <a href="info.php">Info</a>
             </p>
             <p>VASTOUR &copy; 2021 <br/>> made by Lorenzo Jetrin Torquato</p>
             <p></p>
